45ca6995af15742b49af4948f4eb1641247b65fe
fesilva2109 <felipesilvaa.2109@gmail.com>
2025-11-20 17:35:11 -0300

    `Updated router types and API base URL`

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosError, InternalAxiosRequestConfig } from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const API_BASE_URL = 'http://192.168.0.183:8080';
+const API_BASE_URL = 'http://work360-app.azurewebsites.net';
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
